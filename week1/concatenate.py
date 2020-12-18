73c6b467549ceeb9793eb932ea1265125bb0aea2
Suyash <suyash.bidkar@gmail.com>
2020-12-19 01:02:50 +0530

    added concatenate.py

---
+++ week1/concatenate.py
@@ -0,0 +1,10 @@
+#Program for displaying length of joined strings
+
+#taking input
+str1 = input()
+str2 = input()
+
+#concatenation
+str3 = str1 + str2
+
+print(f'Length of concatenated string is: {len(str3)}')
